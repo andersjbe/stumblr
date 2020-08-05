becc324545807fc4896cc1ca3d3bd835609f2457
Ben Anderson <andersjbe@gmail.com>
2020-08-05 09:18:15 -0600

    heroku error handling

--- db/seeders/20200803170658-users_seed.js
+++ db/seeders/20200803170658-users_seed.js
@@ -35,6 +35,6 @@ module.exports = {
       Example:
       return queryInterface.bulkDelete('People', null, {});
     */
-		return queryInterface.bulkDelete('People', null, {});
+		return queryInterface.bulkDelete('Users', null, {});
 	},
 };
