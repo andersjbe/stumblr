925e611ca019a9c4a14f1d76aec0160c12c950a0
Ben Anderson <andersjbe@gmail.com>
2020-08-05 09:14:19 -0600

    heroku error handling

--- frontend/src/Feed.js
+++ frontend/src/Feed.js
@@ -19,9 +19,41 @@ const madeStyles = makeStyles({
 	},
 });
 
+const CardPost = props => {
+	const classes = madeStyles();
+	return (
+		<Card className={classes.root} raised>
+			<CardHeader
+				avatar={
+					<Avatar
+						src='https://64.media.tumblr.com/avatar_998358b1512b_64.pnj'
+						variant='rounded'
+					>
+						B
+					</Avatar>
+				}
+				action={
+					<IconButton>
+						<MoreVert />
+					</IconButton>
+				}
+				title='Some User'
+			/>
+			<CardMedia
+				component='img'
+				src='https://64.media.tumblr.com/d57df59ee95a00cd64e1bad12972d67f/tumblr_pc2un27IF51wzvt9qo1_500.gifv'
+				width='540px'
+			/>
+			<CardContent>
+				<Typography variant='body1'>This is a picture</Typography>
+				<Divider />
+			</CardContent>
+		</Card>
+	);
+};
 
 export default props => {
-    const classes = madeStyles();
+	const classes = madeStyles();
 
 	return (
 		<Card className={classes.root} raised>
